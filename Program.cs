fe01499f7341587d7ab45419ddb55e9ecf2c6f21
MaksimVasilev <126399905+MaksimVasilev@users.noreply.github.com>
2023-03-11 12:06:54 +0300

    HomeWork3

--- Program.cs
+++ Program.cs
@@ -1,24 +1,19 @@
-﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
-//a = 5; b = 7 -> max = 7
-//a = 2 b = 10 -> max = 10
-//a = -9 b = -3 -> max = -3
-
+﻿
 Console.WriteLine("Введите число a");
-int a = Convert.ToInt32 ( Console.ReadLine ());
-Console.WriteLine("Введите число b");
-int b = Convert.ToInt32 ( Console.ReadLine ());
-Console.WriteLine("Введите число c");
-int c = Convert.ToInt32 ( Console.ReadLine ());
+double a = Convert.ToInt32 ( Console.ReadLine ());
+double b = a%2;
 
-int max = a;
-if ( a>b)
- max = a;
-else
- max = b;
+if ( b == 0)
+{
+   Console.Write("Четное");
+}
 
-if (max < c)
-max = c;
+else 
+{
+Console.Write("Нет");
+}
 
-   Console.Write("Max =");
-   Console.Write(max); 
+
+
+
 
