2becc53917c1cafdf1d4438689448f4fa75a4c36
MaksimVasilev <126399905+MaksimVasilev@users.noreply.github.com>
2023-03-19 20:03:26 +0300

    Home

---
+++ HomeWork/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+
+// int x1 = 3;
+// int y1 = 6;
+// int c1 = 8;
+// int x2 = 2;
+// int y2 = 1;
+// int c2 = -7;
+// Coord(x1,x2,y1,y2,c1,c2);
+
+// void Coord ( int x1, int x2,int y1,int y2,int c1,int c2)
+// {
+//    int result1 = (x2-x1)*(x2-x1);
+//    int result2 = (y2-y1)*(y2-y1);
+//    int result3 = (c2-c1)*(c2-c1);
+//    double sum1 = result1+result2+ result3;
+//    double sum =  Math.Sqrt(sum1);
+//    Console.WriteLine(sum);
+// }
+
+
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом. Через строку решать нельзя.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+// try
+// {
+// Console.WriteLine( "введите число");
+// int n = Convert.ToInt32( Console.ReadLine());
+// Check(n);
+
+// void Check(int n)
+// {
+//    int result1 = n%10;
+//    int result2 = n/10000;
+//    int result3 = (n%100)/10;
+//    int result4 = (n%10000)/1000;
+
+// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+// Console.WriteLine("введите число");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+
+// for(int i = 1; i<= n; i++)
+// {
+//     Console.Write ( $"{ i*i*i } ");
+// }
+
--- Program.cs
+++ Program.cs
@@ -1,27 +1,92 @@
-﻿
-Console.WriteLine("Введите число a");
-int a = Convert.ToInt32 ( Console.ReadLine ());
-int b = 1;
-int c = b%2;
+﻿// int a = new Random().Next(100,1000);
+// Console.WriteLine(a);
+// int c = a/100;
+// int b = a%10;
 
-while ( b <= a)
+// Console.Write(c);Console.Write(b);
 
-   {
-      if ( b%2 == 0)
-      {
-        Console.Write( b + " ");
-      b++; 
-      }
-      else
-      {
-         b++;
-      }
-   }
   
+// Console.WriteLine( "введите число c");
+// int c = Convert.ToInt32( Console.ReadLine());
+
+// // if ( c%7 == 0 && c%23 ==0 )
+// // {
+// //    Console.Write ( "Кратно");
+// // }
+// // else
+// // {
+// //    Console.Write ( "No");
+// // }
+
+
+
+// // if ( i == 0)
+// // {
+// //    Console.WriteLine(i);
+// // }
+// // else
+// // {
+// // Console.WriteLine("Кратно");
+// // }
+
+// // Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// // 456 -> 5
+// // 782 -> 8
+// // 918 -> 1
+
+// // Console.WriteLine ("введите трехзначное целое число c");
+// // int c = Convert.ToInt32( Console.ReadLine());
+
+// // int a = c/10;
+// // int b = a%10;
+
+// // Console.Write ( b );
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
 
 
 
 
 
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+// Console.WriteLine ("введите число c от 1 до 7");
+// int c = Convert.ToInt32( Console.ReadLine());
+
+// if ( c < 6 )
+// {
+//    Console.WriteLine ( " На работу ");
+// }
+//    else
+// {
+//    Console.WriteLine ( " Выходной ");
+// }
 
 
+
+string text = "Вы так красноречивы. Вы дадите мне чаю?" ;
+
+// string s = "qwerty"
+//             012
+// s[3] // r
+
+string Replace ( string text, char oldValue, char newValue)
+{
+   string result = string.Empty;
+   int lenght = text.Length;
+   for ( int i = 0; i < lenght; i++)
+   {
+       if( text[i] == oldValue) result = result + $"{newValue}";
+       else result = result + $"{text[i]}";
+   }
+   return result;
+}
+
+string newText = Replace ( text, ' ', '|');
+Console.WriteLine(newText);
